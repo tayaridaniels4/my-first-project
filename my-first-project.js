28592cc6c610da171820f5726e080320018a7a9a
tayari daniels <tayaridaniels4@gmail.com>
2016-09-22 00:57:29 +0000

    my first commit

--- my-first-project.js
+++ my-first-project.js
@@ -1,2 +1,12 @@
 var correct = true
-if (correct{console.log("It's correct!")}else{console.log("It's wrong!")})
+
+if (correct
+{
+    console.log("It's correct!");
+    
+}
+else
+{
+    console.log("It's wrong!");
+    
+}
